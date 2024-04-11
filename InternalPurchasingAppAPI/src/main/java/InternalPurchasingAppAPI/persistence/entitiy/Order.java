f4ef487d8372ca7f1432028c0a634e7ac5de91bf
Brayan Corro <brayan97corro@gmail.com>
2024-04-11 19:27:16 -0400

    commiting fix after removing spring jpa dependency I mistakenly deleted it

---
+++ InternalPurchasingAppAPI/src/main/java/InternalPurchasingAppAPI/persistence/entitiy/Order.java
@@ -0,0 +1,12 @@
+package InternalPurchasingAppAPI.persistence.entitiy;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@Entity
+public class Order {
+}
