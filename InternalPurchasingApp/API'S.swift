9fbe7b08b96da17a0b0860da8c7f399d5c640d45
jannettarred <arredondoj0508@forsythtech.edu>
2024-04-15 21:36:31 -0400

    commit ui

---
+++ InternalPurchasingApp/API'S.swift
@@ -0,0 +1,19 @@
+//
+//  API'S.swift
+//  InternalPurchasingApp
+//
+//  Created by Jannett Arredondo on 4/5/24.
+//
+
+import SwiftUI
+import Alamofire
+
+struct API_S: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    API_S()
+}
