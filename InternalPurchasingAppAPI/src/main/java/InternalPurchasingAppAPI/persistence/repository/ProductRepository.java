d76fdfa9d892aeb2b0c775f7a0d4b44d0bd92ebc
garciae18 <garciae1821@forsythtech.edu>
2024-04-18 23:56:59 -0400

    ignore commit

--- InternalPurchasingAppAPI/src/main/java/InternalPurchasingAppAPI/persistence/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package InternalPurchasingAppAPI.persistence.repository;
-
-import InternalPurchasingAppAPI.persistence.entitiy.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer>{
-}
